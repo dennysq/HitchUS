8dba1821f6b7758f9882b31b4554de479913d991
Dalia22 <dksoleo_22@hotmail.com>
2016-07-16 19:12:43 -0500

    Test

---
+++ HitchUs-web/src/main/java/com/teamj/arquitectura/hitchus/ws/UsuarioRestServices.java
@@ -0,0 +1,58 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.teamj.arquitectura.hitchus.ws;
+
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.UriInfo;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.GET;
+import javax.ws.rs.Produces;
+import javax.enterprise.context.RequestScoped;
+import javax.ws.rs.core.MediaType;
+
+/**
+ * REST Web Service
+ *
+ * @author LENOVO
+ */
+@Path("webresources")
+@RequestScoped
+public class UsuarioRestServices {
+
+    @Context
+    private UriInfo context;
+
+    /**
+     * Creates a new instance of UsuarioRestServices
+     */
+    public UsuarioRestServices() {
+    }
+
+    /**
+     * Retrieves representation of an instance of com.teamj.arquitectura.hitchus.ws.UsuarioRestServices
+     * @return an instance of java.lang.String
+     */
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public String getJson() {
+        //TODO return proper representation object
+        throw new UnsupportedOperationException();
+    }
+
+    /**
+     * PUT method for updating or creating an instance of UsuarioRestServices
+     * @param content representation for the resource
+     */
+    @PUT
+    @Consumes(MediaType.APPLICATION_JSON)
+    public void putJson(String content) {
+    
+    }
+    
+    
+}
